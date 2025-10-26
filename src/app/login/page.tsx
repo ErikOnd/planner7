6ee3b1b355167d3d76a71368a6f0e3955965c0f9
Erik <erik.ondra@yahoo.com>
2025-10-26 10:40:48 +0100

    format

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { createClient } from "@utils/supabase/component";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { createClient } from "@utils/supabase/component";
 
 export default function LoginPage() {
 	const router = useRouter();
