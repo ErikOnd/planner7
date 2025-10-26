1042192e9f88834357677c8cb813593f8a332f71
Erik <erik.ondra@yahoo.com>
2025-10-26 10:38:29 +0100

    renaming

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { createClient } from "../../utils/supabase/component";
+import { createClient } from "@utils/supabase/component";
 
 export default function LoginPage() {
 	const router = useRouter();
