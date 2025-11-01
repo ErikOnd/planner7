655b75926964c56f7e2e775d723294abe4e30e3e
Erik <erik.ondra@yahoo.com>
2025-11-01 10:38:53 +0100

    env variable update

--- src/utils/supabase/static-props.ts
+++ src/utils/supabase/static-props.ts
@@ -2,7 +2,7 @@ import { createClient as createClientPrimitive } from "@supabase/supabase-js";
 
 export function createClient() {
 	return createClientPrimitive(
-		process.env.SUPABASE_URL!,
-		process.env.SUPABASE_PUBLISHABLE_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
 	);
 }
