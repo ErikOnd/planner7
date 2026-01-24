94a4627585adae88d9a1072c70c807621d1d685d
Erik <erik.ondra@yahoo.com>
2026-01-24 09:55:44 +0100

    pwa logo update

--- scripts/generate-favicon.mjs
+++ scripts/generate-favicon.mjs
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import pngToIco from "png-to-ico";
 
 async function generateFavicon() {
-	console.log("Generating favicon.ico...");
 
 	const iconPath = resolve("public/icon-192.png");
 	const outputPath = resolve("src/app/favicon.ico");
