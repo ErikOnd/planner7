bd15ad60970f60963005987ce6f3d4ea1aaf37ab
Erik <erik.ondra@yahoo.com>
2026-02-20 19:40:47 +0100

    eslint config adjustment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
+	...nextCoreWebVitals,
+	...nextTypescript,
+	{
+		rules: {
+			"react-hooks/set-state-in-effect": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	},
 	{
 		ignores: [
 			".next/**",
